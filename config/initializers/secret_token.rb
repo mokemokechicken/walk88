b23448534a51715589734eb5304605ba0321e5b2
Ken Morishita <mokemokechicken@gmail.com>
2013-06-22 12:08:26 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Walk88::Application.config.secret_key_base = 'fcc026dd1d32170201e53a6cd54f686375d23e2d5e4b628c79c053bac8ec59ec973117a0b600ac62ac1504e9c76a0e974eb9915e1856cf9d15fd40dd0447598a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Walk88::Application.config.session_store :cookie_store, key: '_walk88_session'
