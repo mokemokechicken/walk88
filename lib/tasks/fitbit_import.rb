41d000a295c67f2cc788ab53704c0124e90a4ca4
Ken Morishita <mokemokechicken@gmail.com>
2013-06-25 21:07:31 +0900

    fix

--- lib/tasks/fitbit_import.rb
+++ lib/tasks/fitbit_import.rb
@@ -11,18 +11,20 @@ class FitbitImport
         if !setting.fitbit_token.to_s.empty? && !setting.fitbit_secret.to_s.empty?
           record = UserRecord.find_by(user_id: setting.user_id, day: date)
           client.reconnect(setting.fitbit_token, setting.fitbit_secret)
+          p client
           info = Fitbit.fetch_info_by_day(client, day)
+          p info
           if record
             record.update_attributes({
                  steps: info[:step].to_s.to_i,
-                 distance: info[:distance].to_s.to_f
+                 distance: info[:dist].to_s.to_f
             })
           else
             record = UserRecord.create({
                 user_id: setting.user_id,
                 day: date,
                 steps: info[:step].to_s.to_i,
-                distance: info[:distance].to_s.to_f
+                distance: info[:dist].to_s.to_f
                               })
           end
           p record
