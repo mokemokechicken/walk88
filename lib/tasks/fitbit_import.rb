588562f0762337630ef80770b090df7caca1358e
Ken Morishita <mokemokechicken@gmail.com>
2013-06-25 21:15:41 +0900

    fix

--- lib/tasks/fitbit_import.rb
+++ lib/tasks/fitbit_import.rb
@@ -12,10 +12,8 @@ class FitbitImport
           client = Fitbit.create_client
           record = UserRecord.find_by(user_id: setting.user_id, day: date)
           client.reconnect(setting.fitbit_token, setting.fitbit_secret)
-          p client
           info = Fitbit.fetch_info_by_day(client, day)
-          p info
-          if record
+          if record && record.steps < info[:step].to_s.to_i
             record.update_attributes({
                  steps: info[:step].to_s.to_i,
                  distance: info[:dist].to_s.to_f
