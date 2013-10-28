8c65313023a91a1c5c546d8755feeee71ea29188
Ken Morishita <k_morishita@yumemi.co.jp>
2013-10-28 19:39:47 +0900

    fix bug

--- app/controllers/weekly_records_controller.rb
+++ app/controllers/weekly_records_controller.rb
@@ -2,6 +2,6 @@ class WeeklyRecordsController < ApplicationController
   def index
     user_id = params[:user_id].to_i
     weekly_data = UserRecord.weekly_records(user_id)
-    render :json => weekly_data[-[x.size, 10].min..-1]
+    render :json => weekly_data[-[weekly_data.size, 10].min..-1]
   end
 end
