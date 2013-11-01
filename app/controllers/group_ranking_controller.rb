39312ac5fcbbb323aab502b22637cc81d5e43331
Ken Morishita <k_morishita@yumemi.co.jp>
2013-11-01 15:10:33 +0900

    group_ranking: チームのメンバー一覧を表示

--- app/controllers/group_ranking_controller.rb
+++ app/controllers/group_ranking_controller.rb
@@ -3,5 +3,6 @@ class GroupRankingController < ApplicationController
     @user = current_user
     @group_ranking = GroupRanking.ranking_list
     @my_group = GroupUser.find_by(user_id: @user.id)
+    @group_users = GroupUser.all.to_a
   end
 end
