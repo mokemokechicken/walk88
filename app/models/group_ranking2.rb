b2203582b3cdaef53684eee9b3a2ff74d1349afc
Ken Morishita <k_morishita@yumemi.co.jp>
2014-02-14 12:25:39 +0900

    fix: group_ranking span

--- app/models/group_ranking2.rb
+++ app/models/group_ranking2.rb
@@ -1,5 +1,5 @@
 class GroupRanking2 < ActiveRecord::Base
-  START_DATE = '2014-02-01'
+  START_DATE = '2014-02-14'
   END_DATE   = '2014-03-01'
 
   def self.ranking_list(start_date=START_DATE, end_date=END_DATE)
